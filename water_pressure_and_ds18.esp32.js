ad34114d57bfd26a89f4f3c85d7ab1eb2db0ef58
goodkiller <markopraakli@gmail.com>
2020-01-10 11:46:52 +0200

    Update water_pressure_and_ds18.esp32.js

--- water_pressure_and_ds18.esp32.js
+++ water_pressure_and_ds18.esp32.js
@@ -1,8 +1,23 @@
 
+// WIFI
 var _wifi_ssid = 'IoT';
 var _wifi_password = 'IoT12345';
 
+// WebServer
 var _web_port = 80;
+
+// InfluxDB configuration
+var influxDBParams = {
+  influxDBHost: '192.168.1.10',
+  influxPort: 8086,
+  influxDBName: 'zwave',
+  influxUserName: 'asd',
+  influxPassword: 'asd',
+  influxAgentName: getSerial()
+};
+
+
+
 
 var _resolution = 1024;
 var _pinPressureSensor = A0;
@@ -21,23 +36,72 @@ var _sensorReadingTimeout = 5000;
 
 
 var g;
+var pubSensors = [];
 
 
 
 
 var wifi = require('Wifi');
-var http = require('http');
-
+var WebServer = require("WebServer");
+var influxDB = require("InfluxDB").setup(influxDBParams);
 
 console.log('a');
 
 
+function startServer() {
+    var webs = new WebServer({
+        port: _web_port,
+       // file_system: '/var/www/',
+        memory: {
+            'index.html': { 
+                'type': 'text/html',
+                'content': '<html><head><script src="index.js"></script></head><body>' +
+                    '<p>Hello from in memory HTML!</p>' +
+                    '<br><br><button onclick="window.open(\'index.njs\', \'_top\')"> View sensors</button>' +
+                    '<br><p align="right"> <small>Espruino WebServer</small> </p>' +
+                    '</body></html>'
+            },
+            'index.njs': {
+                'content': index_njs
+            }
+        }
+    });
+
+    webs.on('start', function (WebServer) {
+        console.log('WebServer listening on port ' + WebServer.port);
+    });
+    webs.on('request', function (request, response, parsedUrl, WebServer) {
+        console.log('WebServer requested', parsedUrl);
+    });
+    webs.on('error', function (error, WebServer) {
+        console.log('WebServer error', error);
+    });
+
+    webs.createServer();
+}
+
+function index_njs(req, res, uri, webs) {
+  
+    let html = '<html>';
+      html += '<p>Hello from in memory server side javascript!</p>';
+  
+      for (var s in pubSensors)
+      {
+        html += '<p><b>' + s + ': </b>: ' + pubSensors[s] + '</p>';
+      }
+
+    html += '</html>';
+  
+    return {
+        type: 'text/html',
+        content: html
+    };
+}
 
 
 
 
-
-function wifiChecker() {
+function _wifiChecker() {
   
   setInterval(function() {
 
@@ -55,28 +119,26 @@ function wifiChecker() {
   }, 10000); 
 }
 
-// HTTP server (index)
-function processHttpRequest(req, res) {
-  
-  res.writeHead(200);
-  res.end('Hello World');
-}
-
 // Init screen
 function _initScreen()
 {
   I2C1.setup( _oledScreenI2C );
-  g = require("SSD1306").connect( I2C1, _startScreen );
+  g = require("SSD1306").connect( I2C1, _startScreen, { height : 48, width: 64 } );
   
 }
 
 // Start screen
 function _startScreen(){
   
-  require("Font8x16").add(Graphics);
+require("FontDennis8").add(Graphics);
+g.setFontDennis8();
   
-  g.setFont8x16();
-  g.drawString("Ready", 2, 2);
+  console.log(g.getHeight(), g.getWidth() );
+  
+ g.setRotation(1,0);
+  
+ // g.setFont8x16();
+  g.drawString("Ready", 1, 1);
   g.flip();
 }
 
@@ -91,10 +153,11 @@ function _initOneWire()
     let sensors = ow.search().map( device => {
       return require("DS18B20").connect(ow, device);
     });
-
+    
     // Get temperatures
     sensors.forEach((sensor, index) => {
       sensor.getTemp(temp => {
+        pubSensors[sensor.sCode] = temp;
         console.log( sensor.sCode + ": " + temp + "°C" );
       });
     });
@@ -113,8 +176,9 @@ function _initPressureSensor()
         psi = ( resVoltage / _resolution * _maxPressureSensorPressure ),
         bar = psi * 0.0689475728,
         
-        barRounded = Math.round(bar * 100) / 100;
+        barRounded = Math.round(bar * 1000) / 1000;
     
+    pubSensors.pressure = barRounded;
 
     console.log('V:', voltage, 'RES:', resVoltage, 'PSI:', psi, 'BAR:', bar);
     console.log('barRounded -> ', barRounded);
@@ -127,6 +191,30 @@ function _initPressureSensor()
   }, _sensorReadingTimeout );
 }
 
+// Publish sensor info
+function _publishSensorInfo()
+{
+  setInterval(function(){
+    
+    let data = '';
+    
+     for (var s in pubSensors)
+      {
+        data += s + ', device=' + influxDBParams.influxAgentName + ', value=' + pubSensors[s] + "\n";
+      }
+    
+    console.log('Send data: ', data);
+    
+    try {
+     // influxDB.write( data );
+    return true;
+    } catch (e) {
+      console.log(e);
+    }
+    
+  }, 10000);
+}
+
 // Map fn
 function map( x,  in_min,  in_max,  out_min,  out_max){
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -137,15 +225,11 @@ function onInit() {
 
  console.log('b');
   
-  wifiChecker();
-  
    wifi.connect( _wifi_ssid, { password: _wifi_password }, () => {
 
     console.log('Connected to Wifi. IP address is:', wifi.getIP().ip);
      
-     http
-      .createServer(processHttpRequest)
-      .listen( _web_port );
+     startServer();
 
   });
 
@@ -163,6 +247,9 @@ function onInit() {
 
   });
   
+  // Wifi checker
+  _wifiChecker();
+  
   // Init Screen
   _initScreen();
   
@@ -171,6 +258,9 @@ function onInit() {
   
   // Init Pressure sensor
   _initPressureSensor();
+  
+  // Publish sensor info
+  _publishSensorInfo();
 }
 
 function analogReadMedian(p, sampleCount)
